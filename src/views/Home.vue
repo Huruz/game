b4f1a0bfd8be5bf2fcc219598c7f086f5629e5d3
Huruz <huruz_wwe@hotmail.com>
2021-06-15 16:22:07 -0500

    5

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App 4"/>
+    <HelloWorld msg="Welcome to Your Vue.js App 5"/>
   </div>
 </template>
 
